82faa9d925eef5fa98bf38344dafb91c96d4f1c1
Juel Uddin <jueluddin@Juels-MacBook-Pro.local>
2019-02-23 16:40:31 -0500

    adding driver utility class and modify existing test

---
+++ src/main/java/greendesk/greendeskAutomation/DriverUtility.java
@@ -0,0 +1,37 @@
+package greendesk.greendeskAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverUtility
+
+{
+	public WebDriver browser;
+
+	public DriverUtility() {
+		System.setProperty("webdriver.chrome.driver", "/Users/jueluddin/Desktop/chromedriver");
+		browser = new ChromeDriver();
+	}
+
+	public WebDriver getdriver(String browserType) {
+		if (browserType == "chrome") {
+			if (browser == null) {
+				browser = new ChromeDriver();
+				return browser;
+			} else {
+				return browser;
+			}
+
+		} else {
+			if (browser == null) {
+				browser = new FirefoxDriver();
+				return browser;
+			} else {
+				return browser;
+			}
+
+		}
+
+	}
+}
