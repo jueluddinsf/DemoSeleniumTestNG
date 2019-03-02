9a6ed58b14d0ebf9271da53b1fedc93340f79ff0
Umaiz Irfan <umaizirfan@Tahreems-MacBook-Pro.local>
2019-03-02 17:54:20 -0500

    automation

--- src/test/java/greendesk/greendeskAutomation/MyOwnchanges.java
+++ src/test/java/greendesk/greendeskAutomation/MyOwnchanges.java
@@ -2,14 +2,18 @@ package greendesk.greendeskAutomation;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 
-public class MyOwnchanges {
+public class MyOwnchanges extends DriverUtility {
+	WebDriver browser; 
+String base_url = "https://apply.chase.com/mortgage/CRQ/CustomRateQuote.aspx";
   @Test
   public void f() {
+	  // pending
   }
   @BeforeTest
-  public void beforeTest() {
+  public void beforeTest() { 
   }
 
   @AfterTest
