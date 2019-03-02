e254d3c98f68e1f861ea7c1fcfd22f3d18df0a9e
Xavier Mejia <xavier@Xaviers-MacBook-Air.local>
2019-03-02 17:54:27 -0500

    I made a new change

--- src/test/java/greendesk/greendeskAutomation/MyTest.java
+++ src/test/java/greendesk/greendeskAutomation/MyTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
-public class MyTest {
+public class MyTest extends DriverUtility {
   @Test
   public void f() {
   }
