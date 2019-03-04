e92658452e2b765b0e484e1979b2c048edb3f1bc
Xavier Mejia <xavier@xaviers-air.home>
2019-03-03 21:43:49 -0500

    here is more progress on my code for Mytest Branch

--- src/test/java/greendesk/greendeskAutomation/MyTest.java
+++ src/test/java/greendesk/greendeskAutomation/MyTest.java
@@ -5,6 +5,8 @@ import org.testng.annotations.BeforeTest;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.Random;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -39,6 +41,34 @@ public class MyTest extends DriverUtility {
 	  //enter client last name field
 	  browser.findElement(By.id("gmit-gf-last-name")).sendKeys("montana");
 	  
+	  // enter address on text field
+	  Random rand = new Random(System.currentTimeMillis());
+	  int num = rand.nextInt(99); 
+	  int num2 = rand.nextInt(98);
+	  browser.findElement(By.id("address_1")).sendKeys(num + "-" + num2 + " main st");
+	  
+	  //first clear default text on field, then enter City on required City text field
+	  browser.findElement(By.name("salesLead.address.city")).clear();
+	  browser.findElement(By.id("cityname")).sendKeys("Bayside");
+	  
+	  //select state on dropdown on state
+	  
+	  Select State_dropdown_chevy_quote = new Select(browser.findElement(By.id("state")));
+	  State_dropdown_chevy_quote.selectByVisibleText("New York");
+	  
+	  
+	  //enter valid zip code
+	  
+	 browser.findElement(By.id("gmit-gf-postal-code")).clear();
+	 browser.findElement(By.id("gmit-gf-postal-code")).sendKeys("11360");
+	 
+	 
+	  
+	  
+	  
+	  
+	  
+	 
 	  
 	  
 	  
@@ -52,7 +82,7 @@ public class MyTest extends DriverUtility {
 
   @AfterTest
   public void Abort() {
-	  //closeBrowser();
+	  closeBrowser();
 	  //browser.quit();
   }
 
